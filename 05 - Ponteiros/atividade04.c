9743a760e3c5e51d4b08035777008c145ebebd84
Breder Couto <104814239+Cout098@users.noreply.github.com>
2024-06-15 13:49:10 -0300

    atividades

--- 05 - Ponteiros/atividade04.c
+++ 05 - Ponteiros/atividade04.c
@@ -9,6 +9,7 @@ função altera_preco e exiba os dados do produto após a alteração.
 #include <stdio.h>
 #include <locale.h>
 
+#define NUM_PROD 1
 typedef struct
 {
     int codigo;
@@ -16,9 +17,100 @@ typedef struct
     float preco;
 } Produto;
 
+void cadastra_produto(Produto *produto){
+    for (int indiceProduto = 0; indiceProduto < NUM_PROD; indiceProduto++)
+    {
+        printf("Digite o codigo produto: ");
+        scanf("%d", &produto[indiceProduto].codigo);
+        fflush(stdin);
+        printf("Digite o nome do produto: ");
+        gets(produto[indiceProduto].nome);
+        fflush(stdin);
+        printf("Digite o preço do produto: ");
+        scanf("%f", &produto[indiceProduto].preco);
+        fflush(stdin);
+    }
+}
 
+int buscarProduto(){
+    int codBuscado;
+    printf("Qual o Produto você esta buscando?");
+    printf("Buque o produto pelo cod.\n");
+    scanf("%d", &codBuscado);
+
+    return codBuscado;
+}
+
+void altera_preco(Produto *produto, float novo_preco){
+    int indiceProduto;
+    int cod_buscado;
+
+    cod_buscado = buscarProduto();
+    
+    for (indiceProduto = 0; indiceProduto < NUM_PROD; indiceProduto++)
+    {
+        if (cod_buscado == produto[indiceProduto].codigo)
+        {
+            produto[indiceProduto].preco = novo_preco;
+        }
+    }
+}
+
+void exibir_produto(Produto *produto){
+    int indiceProduto;
+    int cod_buscado;
+
+    cod_buscado = buscarProduto();
+
+    for ( indiceProduto = 0; indiceProduto < NUM_PROD; indiceProduto++)
+    {
+        if (cod_buscado == produto[indiceProduto].codigo)
+        {
+            printf("Produto: %d\n", produto[indiceProduto].codigo);
+            printf("Produto: %s\n", produto[indiceProduto].nome);
+            printf("Preço: %.2f\n", produto[indiceProduto].preco);
+        }
+    }
+}
 
 int main(){
+    setlocale(LC_ALL, "Portuguese");
 
+    Produto produto[NUM_PROD];
+    float novo_preco = 0;
+    int opc;
+
+    do
+    {
+        printf("Escolha uma opção: \n");
+        printf("1 - Cadastrar Produto.\n");
+        printf("2 - Alterar Preços.\n");
+        printf("3 - Exibir Produto.\n");
+        printf("4 - Sair.\n");
+        scanf("%d", &opc);
+
+        switch (opc)
+        {
+        case 1:
+            cadastra_produto(produto);
+            break;
+        case 2:
+            altera_preco(produto, novo_preco);
+            break;
+
+        case 3:
+            exibir_produto(produto);
+            break;
+
+        case 4:
+            printf("Press enter");
+            break;
+        
+        default:
+            printf("Opção inválida!");
+            break;
+        }
+    } while (opc != 4);
+    
     return 0;
 }
